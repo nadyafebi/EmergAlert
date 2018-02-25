e3f02307c96d5b8c953f2518d54e3ae7e59580a8
Diane Tobit <dianetobit@gmail.com>
2018-02-25 11:32:21 -0800

    pull message from database

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 /*
   Modules
 */
+
 const express = require('express');
 const path = require('path');
+require('dotenv').config();
+var request = require('request');
+var mongoose = require('mongoose');
 
+var uri = process.env.DB_URL;
+mongoose.connect(uri);
+var db = mongoose.connection;
 /*
   Express Setup
 */
@@ -35,4 +42,23 @@ app.get('/', function(req, res) {
 
 app.get('/test', function(req, res) {
   res.sendFile(html('test'));
+});
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', function() {
+    console.log('MongoDB successfully  connected!');
+});
+
+request('https://api.mlab.com/api/1/databases/athenahacks/collections/messages?apiKey=o2gZ1lynVLWp2xf46oNSrA0avHlH5rUI', { json: true }, (err, res, main) => {
+
+  if (err) { return console.log(err); }
+  console.log(main.length);
+  for(var i = 0; i < main.length; i++){
+    console.log(main[i]["Message"]);
+    
+    // document.getElementById('h4Tag').innerHTML = main[i]["Message"];
+    //$("#panel1").click(function(){
+    //$("h4").append("<p>" + main[i]["Message"] + "</p>");
+//});
+}
 });
